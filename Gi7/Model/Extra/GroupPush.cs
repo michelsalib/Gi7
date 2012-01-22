acb264b67cfe7543b4109b55376de9e7c36c7144
Alberto Monteiro <betimbrasil@gmail.com>
2012-01-22 14:45:01 -0300

    New constructor to BetterObservableCollection Added Extensios Added GroupPush Model

---
+++ Gi7/Model/Extra/GroupPush.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Gi7.Utils;
+
+namespace Gi7.Model.Extra
+{
+    public class GroupPush
+    {
+        public DateTime Date { get; set; }
+        public BetterObservableCollection<Push> Pushes { get; set; }
+
+        public GroupPush()
+        {
+            Pushes = new BetterObservableCollection<Push>();
+        }
+
+        public GroupPush(DateTime date, BetterObservableCollection<Push> pushes)
+        {
+            Date = date;
+            Pushes = pushes;
+        }
+
+        public GroupPush(DateTime date, IEnumerable<Push> pushes)
+        {
+            Date = date;
+            Pushes = new BetterObservableCollection<Push>(pushes);
+        }
+    }
+}
---
+++ Gi7/Utils/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gi7.Utils
+{
+    public static class Extensions
+    {
+        public static DateTime Trunk(this DateTime date)
+        {
+            return date.Subtract(date.TimeOfDay);
+        }
+    }
+}

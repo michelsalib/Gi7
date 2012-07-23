9a613f3dac67373de93af1f4d6d6268c24f5c3ad
Jonathan Best <jonathan.best@student.kit.edu>
2012-07-23 13:14:36 +0200

    implemented create issue in client
    copied most of the code from Gi7.Client.Request.Issue.Comment

---
+++ Gi7.Client/Request/Issue/Create.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Gi7.Client.Request.Base;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Gi7.Client.Request.Issue
+{
+    public class Create : SingleRequest<Model.Issue>
+    {
+        private AddData addData;
+
+        public Create(string username, string repo, string title, string body)
+        {
+            addData = new AddData() { Title = title, Body = body.Replace('\r', '\n') };
+            Uri = String.Format("/repos/{0}/{1}/issues", username, repo);
+        }
+
+        protected override void preRequest(RestSharp.RestClient client, RestSharp.RestRequest request)
+        {
+            request.Method = RestSharp.Method.POST;
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(addData);
+        }
+
+        public class AddData {
+            [JsonProperty("title")]
+            public String Title { get; set; }
+            [JsonProperty("body")]
+            public String Body { get; set; }
+        }
+    }
+}
